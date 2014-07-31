10588316694f731f1943607fc21f907bccd7b54b
Ken Galle <ken@rainshowers.org>
2014-07-30 22:24:30 -0400

    adding headers, todo lists; not done

--- dar_backup.py
+++ dar_backup.py
@@ -1,3 +1,30 @@
+# Script Name: dar_backup.py
+# Author: Ken Galle
+# Description: Adds functionality to the dar utility and in the process makes it
+#    simpler to use, although to do this it does make some assumptions and hides
+#    some functionality (which comes from the enormous list of dar switches).
+#    Note however that a configuration file can be passed thru to dar.  See the
+#    dar man page for details on its structure ("CONDITIONAL SYNTAX").
+#    It manages the creation of incremental backups based on the most recent
+#    available dar file with the same base name in a given directory.
+#    It provides a way to specify a configuration file for dar without worrying
+#    about the current environment (/etc/darrc, ~/.darrc).
+#    It also allows you to pass thru dar prune paths.
+# Note on destination naming convensions:  TODO
+#TODO    Return values:
+#      1xx, where xx is any error value returned from dar during archive creation.
+#      2yy, where yy is any error value returned from dar during archive testing.
+#        dar return values range from 1 to 11 (see dar man page, "EXIT CODES")
+#      Returns 0 on success (both create and test succeeded).
+# TODO:  I need to test for "None" return values in the incremental section.
+# TODO: test for existance of dar and if not found exit with error code.
+# TODO: exit with code if command line params are not correct (in addition to printing usage information)
+# TODO: add support for restoring a batch of archives
+# TODO: packaging: http://blog.ablepear.com/2012/10/bundling-python-files-into-stand-alone.html
+# Version: 1.0
+# Revision History: 
+#       22.08.2005 - Creation
+
 import subprocess
 import logging
 import glob
@@ -58,21 +85,32 @@ for onePath in params['prune']:
     process_strings.append(onePath)
 
 # exclude the destination archive(s) in case they are being created somewhere in the source directory path
+destination_name_without_path = os.path.basename(destination_basename);
 process_strings.append('-X')
-process_strings.append('<destination filename without the path>' + '*.*.dar'
+process_strings.append(destination_name_without_path + '*.*.dar'
 
 # log values from process_strings
+i = 1
+for process_string in process_strings:
+    log.info(str(i) + ' : ' + process_string)
+del(i)
 
 # make call to dar
-return_code = subprocess.call(process_strings, 
-    stdout=subprocess.STDOUT,
-    stderr=subprocess.STDOUT,
-    shell=False)
+return_code = subprocess.call(process_strings, shell=False)
     
 # log return value
+log.info('dar create archive return value=' + str(return_code))
 
 # if not error, then continue on to test
-
-# test
+if return_code = 0:
+    test_process_strings = []
+    test_process_strings.append('/usr/bin/dar')
+    # test archive just created
+    test_process_strings.append('-t')
+    test_process_strings.append(destination_basename)    
+    # make call to dar
+    test_return_code = subprocess.call(test_process_strings, shell=False)
+    # log return value
+    log.info('dar test archive return value=' + str(test_return_code))
   
 
